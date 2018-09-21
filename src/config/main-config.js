5f8b4522468d35f79bd385d79f455ccc00183733
Camila Coder <camilanierocosta@hotmail.com>
2018-09-21 16:38:46 -0400

    cookie secret

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -19,7 +19,7 @@ module.exports = {
         app.use(expressValidator());
         app.use(cookieParser());
         app.use(session({
-            secret: "do not let this get checked into version control",
+            secret: process.env.cookieSecret,
             resave: true,
             saveUninitialized: true,
             cookie: { maxAge: 1.21e+9 }

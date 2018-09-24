deec898549622dd0f280e13208c80b5e9310e028
Camila Coder <camilanierocosta@hotmail.com>
2018-09-24 11:27:24 -0400

    wiki crud and association to user done

---
+++ spec/integration/wikis_spec.js
@@ -0,0 +1,256 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/wikis/";
+
+const sequelize = require("../../src/db/models/index").sequelize;
+const Wiki = require("../../src/db/models").Wiki;
+
+describe("routes : wikis", () => {
+
+    beforeEach((done) => {
+        this.wiki;
+        this.user
+    
+        sequelize.sync({force: true}).then((res) => {
+
+          User.create({
+            email: "starman@tesla.com",
+            password: "Trekkie4lyfe"
+          })
+          .then((user) => {
+            this.user = user;
+    
+            Wiki.create({
+                title: "First Wiki Test",
+                body: "A compilation of reports from recent visits to the star system.",
+                private: false,
+                userId: this.user.id
+            })
+            .then((wiki) => {
+                this.wiki = wiki;
+                done();
+            });
+          })
+  
+        });
+    });
+
+    describe("GET /wikis", () => {
+
+        it("should return a status code 200 and all wikis", (done) => {
+   
+          request.get(base, (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            expect(err).toBeNull();
+            expect(body).toContain("Wikis");
+            expect(body).toContain("First Wiki Test");
+            done();
+          });
+        });
+    });
+
+    describe("GET /wikis/:wikiId/new", () => {
+
+        it("should render a new wiki form", (done) => {
+          request.get(`${base}new`, (err, res, body) => {
+            expect(err).toBeNull();
+            expect(body).toContain("New Wiki");
+            done();
+          });
+        });
+    
+    });
+
+    describe("POST /wikis/create", () => {
+        const options = {
+          url: `${base}create`,
+          form: {
+            title: "blink-182 songs",
+            body: "What's your favorite blink-182 song?"
+          }
+        };
+  
+        it("should create a new wiki and redirect", (done) => {
+  
+          request.post(options,
+  
+            (err, res, body) => {
+              Wiki.findOne({where: {title: "blink-182 songs"}})
+              .then((wiki) => {
+                expect(res.statusCode).toBe(303);
+                expect(wiki.title).toBe("blink-182 songs");
+                expect(wiki.body).toBe("What's your favorite blink-182 song?");
+                done();
+              })
+              .catch((err) => {
+                console.log(err);
+                done();
+              });
+            }
+          );
+        });
+      });
+
+      describe("GET /wikis/:id", () => {
+
+        it("should render a view with the selected wiki", (done) => {
+          request.get(`${base}${this.wiki.id}`, (err, res, body) => {
+            expect(err).toBeNull();
+            expect(body).toContain("First Wiki Test");
+            done();
+          });
+        });
+   
+      });
+
+      describe("POST /wikis/:id/destroy", () => {
+
+        it("should delete the wiki with the associated ID", (done) => {
+
+          Wiki.all()
+          .then((wikis) => {
+   
+            const wikisCountBeforeDelete = wikis.length;
+   
+            expect(wikisCountBeforeDelete).toBe(1);
+   
+            request.post(`${base}${this.wiki.id}/destroy`, (err, res, body) => {
+              Wiki.all()
+              .then((wikis) => {
+                expect(err).toBeNull();
+                expect(wikis.length).toBe(wikisCountBeforeDelete - 1);
+                done();
+              })
+   
+            });
+          });
+   
+        });
+   
+      });
+
+      describe("GET /wikis/:id/edit", () => {
+
+        it("should render a view with an edit wiki form", (done) => {
+          request.get(`${base}${this.wiki.id}/edit`, (err, res, body) => {
+            expect(err).toBeNull();
+            expect(body).toContain("Edit Wiki");
+            expect(body).toContain("First Wiki Test");
+            done();
+          });
+        });
+   
+      });
+
+      describe("POST /wikis/:id/update", () => {
+
+        it("should update the wiki with the given values", (done) => {
+           const options = {
+              url: `${base}${this.wiki.id}/update`,
+              form: {
+                title: "JavaScript Frameworks",
+                body: "There are a lot of them"
+              }
+            };
+
+            request.post(options,
+              (err, res, body) => {
+   
+              expect(err).toBeNull();
+
+              Wiki.findOne({
+                where: { id: this.wiki.id }
+              })
+              .then((wiki) => {
+                expect(wiki.title).toBe("JavaScript Frameworks");
+                done();
+              });
+            });
+        });
+   
+      });
+
+
+
+});
+
+    
+
+
+        //GUEST CONTEXT
+        /*
+        describe("POST /wikis/create", () => {
+            const options = {
+              url: `${base}create`,
+              form: {
+                title: "blink-182 songs",
+                body: "What's your favorite blink-182 song?",
+                userId: this.user.id
+              }
+            };
+      
+            it("should create a new wiki and redirect", (done) => {
+      
+              request.post(options,
+      
+                (err, res, body) => {
+                  Wiki.findOne({where: {title: "blink-182 songs"}})
+                  .then((wiki) => {
+                    expect(wiki).toBeNull();
+                    done();
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    done();
+                  });
+                }
+              );
+            });
+          });
+
+          //ENDS GUEST
+
+          //MEMBER CONTEXT
+    describe("MEMBER attempting to perform CRUD actions for Comment", () => {
+
+        describe("POST /wikis/create", () => {
+            const options = {
+              url: `${base}create`,
+              form: {
+                title: "blink-182 songs",
+                body: "What's your favorite blink-182 song?",
+                userId: this.user.id
+              }
+            };
+      
+            it("should create a new wiki and redirect", (done) => {
+      
+              request.post(options,
+      
+                (err, res, body) => {
+                  Wiki.findOne({where: {title: "blink-182 songs"}})
+                  .then((wiki) => {
+                    expect(wiki).not.toBeNull();
+                    expect(wiki.title).toBe("blink-182 song");
+                    expect(wiki.body).toBe("What's your favorite blink-182 song");
+                    expect(post.userId).not.toBeNull();
+                    done();
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    done();
+                  });
+                }
+              );
+            });
+          });
+          //END MEMBER
+
+    */
+    
+   
+
+    
+  
+    
+  
--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,8 +2,10 @@ module.exports = {
     init(app){
       const staticRoutes = require("../routes/static");
       const userRoutes = require("../routes/users");
+      const wikisRoutes = require("../routes/wikis");
 
       app.use(staticRoutes);
       app.use(userRoutes);
+      app.use(wikisRoutes);
     }
   }
